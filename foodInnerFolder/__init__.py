6e243c162cdb785f2cc3bda5b2aba7b1d991644c
ghorus <nguyen.victor4@gmail.com>
2023-11-30 22:48:54 -0800

    installed psycopg2 binary now

--- foodInnerFolder/__init__.py
+++ foodInnerFolder/__init__.py
@@ -10,7 +10,6 @@ db = SQLAlchemy(app)
 from foodInnerFolder.foodReviews.routes import foodReviewz
 from foodInnerFolder.main.routes import main
 from foodInnerFolder.users.routes import users
-d = os.environ.get('foodSecretKey')
 
 app.register_blueprint(foodReviewz)
 app.register_blueprint(main)

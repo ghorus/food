c2c5ca4b12665e22657ef2cbaebf3fd7c9eda026
ghorus <nguyen.victor4@gmail.com>
2023-11-30 23:04:00 -0800

    take out login form to see problem

--- foodInnerFolder/users/routes.py
+++ foodInnerFolder/users/routes.py
@@ -4,11 +4,11 @@ users = Blueprint('users',__name__)
 
 @users.route("/login",methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash("You've succesfully logged in",'success')
-        return redirect(url_for('main.home'))
-    return render_template('users/login.html',title='Login',form=form)
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     flash("You've succesfully logged in",'success')
+    #     return redirect(url_for('main.home'))
+    return render_template('users/login.html',title='Login')
 
 @users.route("/register",methods=['GET', 'POST'])
 def register():

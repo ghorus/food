ef3d773b41a5e75c4603054e31e2101846a2e4e9
ghorus <nguyen.victor4@gmail.com>
2023-12-04 15:39:22 -0800

    like function completed

--- foodInnerFolder/main/routes.py
+++ foodInnerFolder/main/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template,request
-from foodInnerFolder.models import Post
+from flask import Blueprint,redirect,render_template,request,url_for
+from flask_login import current_user
+from foodInnerFolder import db
+from foodInnerFolder.models import Post,User
 
 main = Blueprint('main',__name__)
 
@@ -8,3 +10,17 @@ def home():
     page = request.args.get('page',1,type=int)
     posts = Post.query.order_by(Post.datePosted.desc()).paginate(page=page,per_page=2)
     return render_template('index.html',posts=posts,title="Home")
+
+@main.route("/<post_id>",methods=['POST'])
+def like(post_id):
+    if current_user.is_authenticated:
+        user = User.query.filter_by(username=current_user.username).first()
+        post = Post.query.filter_by(id=post_id).first()
+    if post in user.likes:
+        user.likes.remove(post)
+        db.session.commit()
+    elif post not in user.likes:
+        user.likes.append(post)
+        db.session.add(user)
+        db.session.commit()
+    return redirect(url_for('main.home'))
---
+++ foodInnerFolder/static/index.js
@@ -0,0 +1,24 @@
+var rs = document.getElementsByClassName("ratingStars")
+Array.from(rs).forEach(rating => {
+    var num = parseInt(rating.innerHTML)
+    rating.innerHTML = ""
+    for(i=0;i<num;i++){
+        const star = document.createElement("img")
+        star.src = "/static/post_pics/star.png"
+        star.style.height = "25px"
+        star.style.backgroundColor = "rgb(220,20,60)"
+        star.style.borderRadius = "5px"
+        star.style.padding = "5px"
+        star.style.margin = "0px 10px 0px 0px"
+        rating.appendChild(star)
+    }
+})
+
+//updateDelete
+const updateDelete = document.getElementsByClassName("updateDeleteContainer")
+Array.from(updateDelete).forEach(menu => {
+    menu.addEventListener("click",()=>{
+        if(menu.offsetHeight==40){menu.style.height="100%"}
+        else{menu.style.height="40px"}
+    })
+})

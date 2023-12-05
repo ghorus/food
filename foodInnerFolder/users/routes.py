dda28f0001d6289b546f041cd0608e6c2ab226b0
ghorus <nguyen.victor4@gmail.com>
2023-12-04 19:36:32 -0800

    normal image logic

--- foodInnerFolder/users/routes.py
+++ foodInnerFolder/users/routes.py
@@ -168,9 +168,7 @@ def save_post_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     random_hex = secrets.token_hex(8)
     picture_fn = random_hex + f_ext
-    upload_folder = 'static/post_pics'
-    app.config['UPLOAD_FOLDER'] = upload_folder
-    picture_path = os.path.join(app.config['UPLOAD_FOLDER'], 'static/post_pics',picture_fn)
+    picture_path = os.path.join(app.root_path, 'static/post_pics',picture_fn)
     i = Image.open(form_picture)
     i.save(picture_path)
     return picture_fn

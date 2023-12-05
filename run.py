d116941f79466db58ac0942327ed3017aa8cc060
ghorus <nguyen.victor4@gmail.com>
2023-12-05 12:43:17 -0800

    change wsgi script again

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 from foodInnerFolder import app,socketio
 import eventlet
 import eventlet.wsgi
-eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('', 8000)),
-                          certfile='cert.crt',
-                          keyfile='private.key',
-                          server_side=True),
-        app)
+eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
 if __name__ == "__main__":
     app.run()

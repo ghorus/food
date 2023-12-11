0353226e96d5f09900d17bf13e8c74d9666b449d
ghorus <nguyen.victor4@gmail.com>
2023-12-11 00:49:33 -0800

    trying stream

--- foodInnerFolder/static/socket.js
+++ foodInnerFolder/static/socket.js
@@ -15,45 +15,55 @@ socket.on('redirect', (dest) => {
     });
 
 
-// const myVideo = document.createElement("video")
-// var peer = new Peer();
-// const videoGrid = document.getElementById("video-grid")
-// myVideo.muted=true
+const myVideo = document.createElement("video")
+var peer = new Peer();
+const peers = {}
+const videoGrid = document.getElementById("video-grid")
+myVideo.muted=true
+var i = []
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio: {
+        echoCancellation: false,
+        autoGainControl: false,
+        noiseCancellation: false
+    }
+}).then(stream => {
+        addVidStream(myVideo,stream)
 
-// const streamer = document.querySelector(".host").innerHTML
-// const viewer = document.querySelector(".viewer").innerHTML
-// if(streamer==viewer){
-//     navigator.mediaDevices.getUserMedia({
-//         video:true,
-//         audio: {
-//             echoCancellation: false,
-//             autoGainControl: false,
-//             noiseCancellation: false
-//         }
-//     }).then(stream => {
-//         addVidStream(myVideo,stream)
-//         socket.on('listen to new users',(id)=>{
-//             console.log(id)
-//             connectToNewUser(id,stream)
-//             function connectToNewUser(id,stream){
-//                 const call = peer.call(id,stream)
-//                 peer.on('call',call=>{
-//                     call.answer(stream)
-//                  })
-//                 //     const vid = document.createElement('video')
-//             }
-//         })
-//         peer.on('open', id => {
-//             socket.emit('listen to new users',id)
-//             });
-//     })
-        
-// function addVidStream(video,stream){
-//     video.srcObject = stream
-//     video.addEventListener('loadedmetadata',()=>{
-//         video.play()
-//     })
-//     videoGrid.append(video)
-//     }
-// }
+    peer.on('call',call =>{
+        call.answer(stream)
+        const vid = document.createElement('video')
+        // call.on('stream',otherGuysStream =>{
+        //     i.push(otherGuysStream.id)
+        //     if(otherGuysStream.id != i[0])
+        //         addVidStream(vid,otherGuysStream)
+        // })
+    })
+    socket.on('listen to new users',(id)=>{
+        connectNewGuy(id,stream)
+    })
+
     
+})
+peer.on('open', id => {
+    socket.emit('listen to new users',id)
+});
+
+function addVidStream(video,stream){
+video.srcObject = stream
+video.addEventListener('loadedmetadata',()=>{
+    video.play()
+})
+videoGrid.append(video)
+}
+function connectNewGuy(id,stream){
+    const call = peer.call(id,stream)
+    const vid = document.createElement('video')
+    call.on('stream',remoteStream =>{
+        addVidStream(vid,remoteStream)
+    })
+    call.on('close',()=>{
+        vid.remove()
+    })
+}
--- foodInnerFolder/streaming/routes.py
+++ foodInnerFolder/streaming/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint,render_template,request,session,redirect,url_for
-from flask_socketio import leave_room, join_room,send,emit
+from flask_socketio import emit
 from foodInnerFolder import socketio,app
-import random
-from string import ascii_uppercase
+
 
 stream = Blueprint('stream',__name__)
 
@@ -14,3 +13,6 @@ def streaming(host):
 def new_user_connected(user_id):
     emit('listen to new users',user_id,broadcast=True)
 
+@socketio.on("user dc")
+def user_disconnected(user_id):
+    emit('user dc',user_id)

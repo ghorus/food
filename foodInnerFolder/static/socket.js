7706c0338d24fdb6e38a0da196f19f9094e59419
ghorus <nguyen.victor4@gmail.com>
2023-12-10 20:14:52 -0800

    commented out stream

--- foodInnerFolder/static/socket.js
+++ foodInnerFolder/static/socket.js
@@ -15,50 +15,50 @@ var socket = io();
         });
 
 //streaming
-const myVideo = document.createElement("video")
-var peer = new Peer();
-const videoGrid = document.getElementById("video-grid")
-myVideo.muted=true
+// const myVideo = document.createElement("video")
+// var peer = new Peer();
+// const videoGrid = document.getElementById("video-grid")
+// myVideo.muted=true
 
-const streamer = document.querySelector(".host").innerHTML
-const viewer = document.querySelector(".viewer").innerHTML
-if(streamer==viewer){
-    navigator.mediaDevices.getUserMedia({
-        video:true,
-        audio: {
-            echoCancellation: false,
-            autoGainControl: false,
-            noiseCancellation: false
-        }
-    }).then(stream => {
-        addVidStream(myVideo,stream)
-        socket.on('listen to new users',(id)=>{
-            console.log(id)
-            connectToNewUser(id,stream)
-            function connectToNewUser(id,stream){
-                const call = peer.call(id,stream)
-                //     const vid = document.createElement('video')
-                call.on('stream',userVidStream=>{
-                    // addVidStream(vid,userVidStream)
-                    console.log('calling ' + id, userVidStream)
-                })
-                //     call.on('close',()=>{
-                //         vid.remove()
-                //         console.log('user left')
-                //     })
-            }
-        })
-        peer.on('open', id => {
-            socket.emit('listen to new users',id)
-            });
-    })
+// const streamer = document.querySelector(".host").innerHTML
+// const viewer = document.querySelector(".viewer").innerHTML
+// if(streamer==viewer){
+//     navigator.mediaDevices.getUserMedia({
+//         video:true,
+//         audio: {
+//             echoCancellation: false,
+//             autoGainControl: false,
+//             noiseCancellation: false
+//         }
+//     }).then(stream => {
+//         addVidStream(myVideo,stream)
+//         socket.on('listen to new users',(id)=>{
+//             console.log(id)
+//             connectToNewUser(id,stream)
+//             function connectToNewUser(id,stream){
+//                 const call = peer.call(id,stream)
+//                 //     const vid = document.createElement('video')
+//                 call.on('stream',userVidStream=>{
+//                     // addVidStream(vid,userVidStream)
+//                     console.log('calling ' + id, userVidStream)
+//                 })
+//                 //     call.on('close',()=>{
+//                 //         vid.remove()
+//                 //         console.log('user left')
+//                 //     })
+//             }
+//         })
+//         peer.on('open', id => {
+//             socket.emit('listen to new users',id)
+//             });
+//     })
         
-    function addVidStream(video,stream){
-        video.srcObject = stream
-        video.addEventListener('loadedmetadata',()=>{
-            video.play()
-        })
-        videoGrid.append(video)
-        }
-}
+//     function addVidStream(video,stream){
+//         video.srcObject = stream
+//         video.addEventListener('loadedmetadata',()=>{
+//             video.play()
+//         })
+//         videoGrid.append(video)
+//         }
+// }
      
